75b94a2fe3aeb007854a498fb3a7b7c6db68f611
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2025-10-20 09:02:43 +0700

    feat: add type module

---
+++ src/modules/type.ts
@@ -0,0 +1,4 @@
+export type Command = "L" | "R" | "M";
+export type Direction = "N" | "E" | "S" | "W";
+export type Positon = [number, number];
+export type Status = "Success" | "Obstacle encountered" | "Out of bounds";
