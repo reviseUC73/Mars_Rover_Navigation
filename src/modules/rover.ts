578c2470a0f6192e277da34f7fc3d590321b1d97
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2025-10-20 19:44:09 +0700

    feat: add rover module

---
+++ src/modules/rover.ts
@@ -0,0 +1,77 @@
+import type { Directions, Positon, RoverStatus } from "./type.js";
+import type { Grid } from "./grid.js";
+
+// Handles position, direction, and movement logic on grid
+export class Rover {
+  private currentPositon: Positon;
+  private currentDirection: Directions;
+  private grid: Grid;
+  private readonly leftDirectionMap: Record<Directions, Directions> = {
+    N: "W",
+    W: "S",
+    S: "E",
+    E: "N",
+  };
+  private readonly rightDirectionMap: Record<Directions, Directions> = {
+    N: "E",
+    W: "N",
+    S: "W",
+    E: "S",
+  };
+  constructor(
+    currentPositon: Positon = [0, 0],
+    currentDirection: Directions = "N",
+    grid: Grid
+  ) {
+    this.currentDirection = currentDirection;
+    this.currentPositon = currentPositon;
+    this.grid = grid;
+  }
+
+  getPositon(): Positon {
+    return this.currentPositon;
+  }
+
+  getDirection(): Directions {
+    return this.currentDirection;
+  }
+
+  turnLeft(): void{
+    this.currentDirection = this.leftDirectionMap[this.currentDirection];
+  }
+
+  turnRight(): void {
+    this.currentDirection = this.rightDirectionMap[this.currentDirection];
+  }
+
+  move(): void | RoverStatus {
+    const nextPosition = this.movePostion(this.currentPositon,this.currentDirection)
+    
+    if(this.grid.hasObstacles(nextPosition)){
+        return "Obstacle encountered"
+    }
+    else if(!this.grid.inBounds(nextPosition)){
+        return "Out of bounds"
+    }
+    this.currentPositon = nextPosition
+  }
+
+  private movePostion(position: Positon, direction: Directions): Positon {
+    const [x, y] = position;
+    const [dx, dy] = this.toDirection(direction);
+    return [x + dx, y + dy];
+  }
+
+  private toDirection(targetDirection: Directions): Positon {
+    switch (targetDirection) {
+      case "N":
+        return [0, 1];
+      case "E":
+        return [1, 0];
+      case "S":
+        return [0, -1];
+      case "W":
+        return [-1, 0];
+    }
+  }
+}
